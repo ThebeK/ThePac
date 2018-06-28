65d3243b3743cae313319c8d4bab33756284c5d4
16101163 <37653478+16101163@users.noreply.github.com>
2018-06-28 05:13:27 +0200

    Add files via upload
    5:13

--- Global.asax
+++ Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartPac_v1._0.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartPac_v1._1.MvcApplication" Language="C#" %>
--- Global.asax.cs
+++ Global.asax.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Mvc;
+using System.Web.Routing;
 
-namespace SmartPac_v1._0
+namespace SmartPac_v1._1
 {
-    public class Global : System.Web.HttpApplication
+    public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start(object sender, EventArgs e)
+        protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
